3f856eb7b5b8b4128fd6b349ce0b5e439aeae836
yevhen <yevhen.krasnokutsky@gmail.com>
2022-12-20 16:34:42 +0200

    3 2 1 0...

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(custom-safe-themes
+   '("2078837f21ac3b0cc84167306fa1058e3199bbd12b6d5b56e3777a4125ff6851" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" default))
+ '(menu-bar-mode nil)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
---
+++ keys.emacs
@@ -0,0 +1,25 @@
+;; https://gist.github.com/hjertnes/9e14416e8962ff5f03c6b9871945b165
+;; https://naghdbishi.ir/Doom-Emacs-Cheat-Sheet/README.html
+
+
+;; https://github.com/gabesoft/evil-mc
+g z z - multicursor
+g z d - multicursor in duplicates
+g z m - Make a cursor for every match of the selected region or the symbol at point
+
+M-d - select duplicates, no ability to edit out of region of similarity
+v, SHIFT+R - select all duplicates at once, no ability to edit out region of similarity
+
+M-SHIFT-% - find and replace
+
+u - undo
+C-r - redu
+C-x h - select all
+gg V G - select all
+
+g d - go to definition
+C-o - go to the prev cursor position
+
+C-x C-; - comment / uncomment
+
+SPC h t - load theme "whiteboard" "doom-one", "doom-one-light"
